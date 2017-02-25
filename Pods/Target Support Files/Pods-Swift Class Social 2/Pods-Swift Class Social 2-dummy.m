b05e867db1a8afe13f4f5083325c93a82b06b71e
Essacnewton <bgicewolves@gmail.com>
2017-02-25 10:03:14 -0500

    UI transfered over and all pods installed

---
+++ Pods/Target Support Files/Pods-Swift Class Social 2/Pods-Swift Class Social 2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift_Class_Social_2 : NSObject
+@end
+@implementation PodsDummy_Pods_Swift_Class_Social_2
+@end
---
+++ Swift Class Social 2/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  Swift Class Social 2
+//
+//  Created by Isaac Prunty on 2/25/17.
+//  Copyright © 2017 Isaac Prunty. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
