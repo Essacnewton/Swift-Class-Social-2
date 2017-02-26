44cc018fd0d0bf5607ca02d78868ad846a538a9e
Essacnewton <bgicewolves@gmail.com>
2017-02-26 11:24:43 -0500

    feed-table is setup

---
+++ Swift Class Social 2/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Swift Class Social 2
+//
+//  Created by Isaac Prunty on 2/25/17.
+//  Copyright © 2017 Isaac Prunty. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+   
+
+}
